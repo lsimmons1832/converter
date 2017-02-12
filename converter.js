28a16b8ee0914cba734abcac607c69cefa6921b5
Latasha Simmons <lsimmons1832@gmail.com>
2017-02-12 16:12:11 -0600

    detect radio button

--- converter.js
+++ converter.js
@@ -12,8 +12,14 @@ function toFahrenheit () {
 
 
 // This function should determine which conversion should
-// happen based on which radio button is selected.
+// happen based on which radio button that is selected.
 function determineConverter (clickEvent) {
+	if document.getElementById("Celsius").checked === true) {
+	toFahrenheit ();
+}
+	else document.getElementById("Fahrenheit").checked) {
+	toCelsius ();
+}
   console.log("event", clickEvent);
 }
 
